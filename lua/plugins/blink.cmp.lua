e4623b7c4fa3e79f1579d534cbca773efa33186b
Kevin Hamer <kevin@imarc.com>
2024-12-23 15:18:28 -0500

    update blink.cmp and nvim-treesitter configs

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -6,45 +6,18 @@ return {
   lazy = false,
   version = 'v0.*',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
-    keymap = { preset = 'super-tab' },
-
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = true,
-    },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'mono',
-
-    -- experimental auto-brackets support
-    -- accept = { auto_brackets = { enabled = true } }
-
-    -- experimental signature help support
-    -- trigger = { signature_help = { enabled = true } }
-
     sources = {
-        providers = {
-            copilot = {
-                name = "copilot",
-                module = "blink-cmp-copilot",
-            },
+      default = { "lsp", "path", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
         },
-        completion = {
-            enabled_providers = { "lsp", "path", "buffer", "copilot" }
-        },
+      },
     },
+    signature = { enabled = true },
   },
-  -- allows extending the enabled_providers array elsewhere in your config
-  -- without having to redefining it
-  opts_extend = { "sources.completion.enabled_providers" }
 }
