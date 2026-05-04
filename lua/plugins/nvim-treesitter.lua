5863fcc0f9c2832dc86fd25bf2f1920451eb3851
Kevin Hamer <kevin@imarc.com>
2026-05-04 13:43:33 -0400

    fix md bug

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = { 'markdown', 'markdown_inline' },
+  },
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
+  end,
 }
